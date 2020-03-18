db6ff2c45abf2c3462769a0102940cbd480b8f4e
MtDeity <o8olo853q43@gmail.com>
2020-03-18 15:04:57 +0900

    What is State and How do we Manage it?

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,68 +1,52 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
         'Buy Milk',
+        style: TextStyle(
+          decoration:
+              isChecked ? TextDecoration.lineThrough : TextDecoration.none,
+        ),
       ),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value) {},
+      trailing: TaskCheckbox(
+        checkboxState: isChecked,
+        toggleCheckbox: (bool checkboxState) {
+          setState(() {
+            isChecked = checkboxState;
+          });
+        },
       ),
     );
   }
 }
 
-class ToDoRow extends StatefulWidget {
-  ToDoRow(
-      {@required this.text,
-      @required this.checked,
-      @required this.lineThrough});
-  final String text;
-  final bool checked;
-  final bool lineThrough;
-
-  @override
-  _ToDoRowState createState() => _ToDoRowState();
-}
-
-class _ToDoRowState extends State<ToDoRow> {
-  String text;
-  bool checked;
-  bool lineThrough;
-
-  @override
-  void initState() {
-    super.initState();
-    text = widget.text;
-    checked = widget.checked;
-    lineThrough = widget.lineThrough;
-  }
+class TaskCheckbox extends StatelessWidget {
+  TaskCheckbox({this.checkboxState, this.toggleCheckbox});
+  final bool checkboxState;
+  final Function toggleCheckbox;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Text(
-          text,
-          style: TextStyle(
-            fontSize: 18.0,
-            decoration:
-                lineThrough ? TextDecoration.lineThrough : TextDecoration.none,
-          ),
-        ),
-        Checkbox(
-          value: checked,
-          onChanged: (value) {
-            setState(() {
-              checked = value;
-            });
-          },
-        ),
-      ],
+    return Checkbox(
+      value: checkboxState,
+      onChanged: toggleCheckbox,
     );
   }
 }
+
+//ToDo 3. isCheckedなら打ち消し線引きたいのでisCheckedをTaskTileに移す、TaskTileを右クリックでStatefulに、TaskCheckboxはStatelessにする
+//ToDo 4. TaskCheckboxのコンストラクタにcheckboxStateを作る、isCheckedをcheckboxStateで置き換えるがsetStateはひとまずコメントアウトしておく
+//ToDo 5. TextStyleにdecorationでlineThroughつける、isCheckedなら打ち消し、falseならnull
+//ToDo 6. TaskCheckboxのsetStateをcheckboxCallbackとしてTaskTileの関数にする、引数はcheckboxState
+//ToDo 7. TaskCheckboxのコンストラクタにfinal Function toggleCheckBoxを加える
+//ToDo 8. 引数には無名関数で入れ、checkboxCallbackは消す
