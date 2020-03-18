99351f69364edd2a092398be0468ca229cec2d87
MtDeity <o8olo853q43@gmail.com>
2020-03-18 19:53:44 +0900

    Using a Provider and a ChangeNotifier to Manage State

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,27 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:todoey_flutter/models/task.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey_flutter/models/task_data.dart';
 import 'package:todoey_flutter/widgets/task_tile.dart';
 
-class TasksList extends StatefulWidget {
-  final List<Task> tasks;
-  TasksList(this.tasks);
-  @override
-  _TasksListState createState() => _TasksListState();
-}
-
-class _TasksListState extends State<TasksList> {
+class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: widget.tasks.length,
-      itemBuilder: (context, index) {
-        return TaskTile(
-          taskTitle: widget.tasks[index].name,
-          checkboxState: widget.tasks[index].isDone,
-          checkboxCallback: (checkboxState) {
-            setState(() {
-              widget.tasks[index].toggleDone();
-            });
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+          itemCount: taskData.tasks.length,
+          itemBuilder: (context, index) {
+            return TaskTile(
+              taskTitle: taskData.tasks[index].name,
+              checkboxState: taskData.tasks[index].isDone,
+              checkboxCallback: (checkboxState) {
+//            setState(() {
+//              widget.tasks[index].toggleDone();
+//            });
+              },
+            );
           },
         );
       },
