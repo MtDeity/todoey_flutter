f467169c7379c57676390a6b8da1d2097cec3f75
MtDeity <o8olo853q43@gmail.com>
2020-03-18 16:39:03 +0900

    The ListView Builder

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,52 +1,54 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatefulWidget {
-  @override
-  _TaskTileState createState() => _TaskTileState();
-}
-
-class _TaskTileState extends State<TaskTile> {
-  bool isChecked = false;
+class TaskTile extends StatelessWidget {
+  TaskTile({this.taskTitle, this.checkboxState, this.checkboxCallback});
+  final String taskTitle;
+  final bool checkboxState;
+  final Function checkboxCallback;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
-        'Buy Milk',
+        taskTitle,
         style: TextStyle(
           decoration:
-              isChecked ? TextDecoration.lineThrough : TextDecoration.none,
+              checkboxState ? TextDecoration.lineThrough : TextDecoration.none,
         ),
       ),
-      trailing: TaskCheckbox(
-        checkboxState: isChecked,
-        toggleCheckbox: (bool checkboxState) {
-          setState(() {
-            isChecked = checkboxState;
-          });
-        },
+      trailing: Checkbox(
+        value: checkboxState,
+        onChanged: checkboxCallback,
       ),
     );
   }
 }
 
-class TaskCheckbox extends StatelessWidget {
-  TaskCheckbox({this.checkboxState, this.toggleCheckbox});
-  final bool checkboxState;
-  final Function toggleCheckbox;
+//          setState(() {
+//            isChecked = checkboxState;
+//          });
 
-  @override
-  Widget build(BuildContext context) {
-    return Checkbox(
-      value: checkboxState,
-      onChanged: toggleCheckbox,
-    );
-  }
-}
+//class TaskCheckbox extends StatelessWidget {
+//  TaskCheckbox({this.checkboxState, this.toggleCheckbox});
+//  final bool checkboxState;
+//  final Function toggleCheckbox;
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Checkbox(
+//      value: checkboxState,
+//      onChanged: toggleCheckbox,
+//    );
+//  }
+//}
 
-//ToDo 3. isCheckedなら打ち消し線引きたいのでisCheckedをTaskTileに移す、TaskTileを右クリックでStatefulに、TaskCheckboxはStatelessにする
-//ToDo 4. TaskCheckboxのコンストラクタにcheckboxStateを作る、isCheckedをcheckboxStateで置き換えるがsetStateはひとまずコメントアウトしておく
-//ToDo 5. TextStyleにdecorationでlineThroughつける、isCheckedなら打ち消し、falseならnull
-//ToDo 6. TaskCheckboxのsetStateをcheckboxCallbackとしてTaskTileの関数にする、引数はcheckboxState
-//ToDo 7. TaskCheckboxのコンストラクタにfinal Function toggleCheckBoxを加える
-//ToDo 8. 引数には無名関数で入れ、checkboxCallbackは消す
+//ToDo 1. ToDoを追加できるようにしていく、まずTaskListをstfulにする、tasksというリストのプロパティを作る
+//ToDo 2. modelsフォルダにtask.dartを作る、コンストラクタはfinal String nameとbool isDone、isDoneはデフォルトでfalseにしておく
+//ToDo 3. toggleDoneメソッドを作る、isDoneに逆を入れる
+//ToDo 4. tasksリストにTaskインスタンスを入れていく
+//ToDo 5. TaskTileをstlessにする、isCheckedをfinalに、setStateを逃しておいてcheckboxの分割やめる、valueはisChecked、onChangedは一時コメントアウト
+//ToDo 6. TaskTileのコンストラクタはisCheckedとtaskTitle
+//ToDo 7. TasksListにいちいちtasksから引っ張ってくるのではなくListView.builderを返すようにする
+//ToDo 8. itemBuilderの無名関数の引数はcontext, index、returnでTaskTileを返す、プロパティは`tasks[index].name`のような指定ができる
+//ToDo 9. itemBuilderの他にitemCountが必要、tasksの長さ入れる
+//ToDo 10. TaskTileのコンストラクタにcheckboxCallback加える、インスタンス生成時に(checkboxState)を引数にsetState内でtoggleDoneする無名関数を加える
